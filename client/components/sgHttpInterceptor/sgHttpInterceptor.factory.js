9c2583d0b43d4df313b6c231b8a507b5aa769197
Dae-sang Cho <cdsleaf@naver.com>
2016-08-16 22:44:34 +0900

    http interceptor factory 진행 중.

--- client/components/sgHttpInterceptor/sgHttpInterceptor.factory.js
+++ client/components/sgHttpInterceptor/sgHttpInterceptor.factory.js
@@ -6,7 +6,7 @@
     .factory('sgHttpInterceptor', sgHttpInterceptor);
 
   /* @ngInject */
-  function sgHttpInterceptor() {
+  function sgHttpInterceptor($q) {
     return{
       'request':request,
       'requestError':requestError,
@@ -19,14 +19,37 @@
       return config;
     }
 
-    function requestError() {
+    function requestError(rejection) {
 
+      var msg = httpType(rejection.status);
+      sgAlert.error(msg.value, msg.code);
       //에러처리
       return $q.reject(rejection);
+    }
 
+    function response(response) {
+      return response;
+    }
+
+    function responseError(rejection) {
+      return $q.reject(rejection);
+    }
+
+    function httpType(status){
+      var msg = null;
+      if(status === 400){
+        msg = {
+          code : 'BAD_REQUEST',
+          value:' YOU send a bad request. send the right thing'
+        };
+      }else if(status === 401){
+
+      }
+
+      return msg;
     }
   }
-})
+});
 
 
 
